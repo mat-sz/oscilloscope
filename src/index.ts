d39769e5d496c9b4a63f8aa34844c21d8298d147
Mat Sz <contact@matsz.dev>
2020-02-23 17:36:31 +0100

    created a sample type

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './App.scss';
 
-type Samples = { x: number, y: number }[];
+type Sample = { x: number, y: number };
 
 const audioContext = new AudioContext();
 const audioElement = document.getElementById('audio') as HTMLMediaElement;
@@ -12,14 +12,13 @@ source.connect(processor);
 processor.connect(gain);
 gain.connect(audioContext.destination);
 
-let oldSamples: Samples[] = [];
-let buffer: AudioBuffer = undefined;
+let oldSamples: Sample[][] = [];
 processor.addEventListener('audioprocess', (e) => {
     // Copying the output back so we can hear the audio.
     e.outputBuffer.copyToChannel(e.inputBuffer.getChannelData(0), 0);
     e.outputBuffer.copyToChannel(e.inputBuffer.getChannelData(1), 1);
 
-    let samples: { x: number, y: number }[] = [];
+    let samples: Sample[] = [];
     const leftChannel = e.inputBuffer.getChannelData(0);
     const rightChannel = e.inputBuffer.getChannelData(1);
 
@@ -38,14 +37,13 @@ const ctx = canvas.getContext('2d');
 
 function draw() {
     ctx.fillStyle = 'rgb(5, 5, 5)';
-    ctx.fillRect(0, 0, 800, 800);
-    ctx.fillStyle = 'rgb(0, 255, 0)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     for (let i = oldSamples.length - 1; i >= 0; i--) {
         ctx.fillStyle = 'rgb(0, ' + (i * 30) + ', 0)';
         
         for (let sample of oldSamples[i]) {
-            ctx.fillRect((sample.x + 1) * canvas.width/2, (sample.y + 1) * canvas.height/2, 2, 2);
+            ctx.fillRect((sample.x + 1) * canvas.width/2, (sample.y + 1) * canvas.height/2, 1, 1);
         }
     }
 
