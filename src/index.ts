2fe794471ee39fc1056f852fea9e06d05daba5f4
Mat Sz <contact@matsz.dev>
2020-02-23 17:25:46 +0100

    improved accuracy

--- src/index.ts
+++ src/index.ts
@@ -12,45 +12,40 @@ source.connect(processor);
 processor.connect(gain);
 gain.connect(audioContext.destination);
 
+let oldSamples: Samples[] = [];
 let buffer: AudioBuffer = undefined;
 processor.addEventListener('audioprocess', (e) => {
-    buffer = e.inputBuffer;
-
     // Copying the output back so we can hear the audio.
     e.outputBuffer.copyToChannel(e.inputBuffer.getChannelData(0), 0);
     e.outputBuffer.copyToChannel(e.inputBuffer.getChannelData(1), 1);
+
+    let samples: { x: number, y: number }[] = [];
+    const leftChannel = e.inputBuffer.getChannelData(0);
+    const rightChannel = e.inputBuffer.getChannelData(1);
+
+    for (let i = 0; i < leftChannel.length; i++) {
+        samples.push({ x: leftChannel[i], y: -rightChannel[i] });
+    }
+
+    oldSamples.push(samples);
+    if (oldSamples.length > 10) {
+        oldSamples.shift();
+    }
 })
 
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
-let oldSamples: Samples[] = [];
 
 function draw() {
     ctx.fillStyle = 'rgb(5, 5, 5)';
     ctx.fillRect(0, 0, 800, 800);
     ctx.fillStyle = 'rgb(0, 255, 0)';
 
-
-    if (buffer) {
-        let samples: { x: number, y: number }[] = [];
-        const leftChannel = buffer.getChannelData(0);
-        const rightChannel = buffer.getChannelData(1);
-
-        for (let i = 0; i < leftChannel.length; i++) {
-            samples.push({ x: leftChannel[i], y: rightChannel[i] });
-        }
-
-        oldSamples.push(samples);
-        if (oldSamples.length > 30) {
-            oldSamples.shift();
-        }
-
-        for (let i = oldSamples.length - 1; i >= 0; i--) {
-            ctx.fillStyle = 'rgb(0, ' + (i * 10) + ', 0)';
-            
-            for (let sample of oldSamples[i]) {
-                ctx.fillRect((sample.x + 1) * canvas.width/2, (sample.y + 1) * canvas.height/2, 2, 2);
-            }
+    for (let i = oldSamples.length - 1; i >= 0; i--) {
+        ctx.fillStyle = 'rgb(0, ' + (i * 30) + ', 0)';
+        
+        for (let sample of oldSamples[i]) {
+            ctx.fillRect((sample.x + 1) * canvas.width/2, (sample.y + 1) * canvas.height/2, 2, 2);
         }
     }
 
